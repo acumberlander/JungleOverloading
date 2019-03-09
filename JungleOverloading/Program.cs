a22e5fcb69f5867eaf1f87c66fec41c998fddc56
Austin <austincumberlander@gmail.com>
2019-03-09 02:33:40 -0600

    done

--- JungleOverloading/Program.cs
+++ JungleOverloading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JungleOverloading
 {
@@ -6,7 +7,35 @@ namespace JungleOverloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Heading
+            Console.WriteLine("Jungle Overloading Assignment:");
+            Console.WriteLine("");
+
+            // Employees
+            Employee employee1 = new Employee("John", "Smith");
+            Employee employee2 = new Employee("Austin", "Cumberlander");
+            Employee employee3 = new Employee("James", "Brown");
+            Employee employee4 = new Employee("Andrea", "Ellison");
+
+            // Employee List
+            // How to explicitly set objects in list without having to use the Add method?
+            var companions = new List<Employee>();
+            companions.Add(employee1);
+            companions.Add(employee2);
+            companions.Add(employee3);
+
+            // Methods
+            employee1.eat();
+            Console.WriteLine("");
+
+            employee2.eat("ramen");
+            Console.WriteLine("");
+
+            employee3.eat(companions);
+            Console.WriteLine("");
+
+            employee4.eat("steak", companions);
+            Console.ReadLine();
         }
     }
 }
